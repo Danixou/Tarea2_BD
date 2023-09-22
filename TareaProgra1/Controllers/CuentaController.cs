f3197694a87dd978f5798c46c536b68477137973
Oseec <113547766+Oseec@users.noreply.github.com>
2023-09-22 15:48:14 +0200

    avance 2 login

--- TareaProgra1/Controllers/CuentaController.cs
+++ TareaProgra1/Controllers/CuentaController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
 using System.Security.Claims;
 using TareaProgra1.Data;
+using TareaProgra1.Models;
 
 namespace TareaProgra1.Controllers
 {
@@ -27,7 +31,58 @@ namespace TareaProgra1.Controllers
 
         [HttpPost]
 
-        public async Task<IActionResult> Logi
+        public async Task<IActionResult> Login(Usuario usuario)
+        {
+            try
+            {
+                using (SqlConnection con = new(_bdcontext.Conexion))
+                {
+                    using (SqlCommand cmd = new("sp_ValidarUsuario", con))
+                    {
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        cmd.Parameters.Add("@UserName", System.Data.SqlDbType.VarChar).Value = usuario.UserName;
+                        cmd.Parameters.Add("@Password", System.Data.SqlDbType.VarChar).Value = usuario.Password;
+                        con.Open();
+                        var dr = cmd.ExecuteReader();
+                        while (dr.Read())
+                        {
+                            if (dr["UserName"] != null && usuario.UserName != null)
+                            {
+                                List<Claim> c = new List<Claim>()
+                                {
+                                    new Claim(ClaimTypes.NameIdentifier, usuario.UserName)
+                                };
+                                ClaimsIdentity ci = new(c, CookieAuthenticationDefaults.AuthenticationScheme);
+                                AuthenticationProperties p = new();
+
+                                p.AllowRefresh = true;
+                                p.IsPersistent = usuario.MantenerActivo;
+
+                                if (!usuario.MantenerActivo)
+                                    p.ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(1);
+                                else
+                                    p.ExpiresUtc = DateTimeOffset.UtcNow.AddDays(1);
+
+                                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(ci), p);
+                                return RedirectToAction("Index", "Home");
+                            }
+                            else
+                            {
+                                ViewBag.Error = "Credenciales incorrectas o cuenta no registrada.";
+                            }
+                        }
+                        con.Close();
+                    }
+                    return View();
+                }
+            }
+            catch(System.Exception e)
+            {
+                ViewBag.Error = e.Message;
+                return View();
+            }
+
+        }
 
     }
 }
